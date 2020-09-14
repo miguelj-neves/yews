f33430c413064b90cd6ceb7a5a3eb3f691992bd1
Miguel Neves <36202483+miguelj-neves@users.noreply.github.com>
2020-09-14 12:30:59 -0400

    Add CpicIberia

--- src/yews/models/cpic.py
+++ src/yews/models/cpic.py
@@ -4,7 +4,7 @@ from torch import load
 
 from .utils import load_state_dict_from_url
 
-__all__ = ['CpicV1', 'CpicV2', 'cpic_v1', 'cpic_v2','cpicv1_dropout', 'CpicDropout']
+__all__ = ['CpicV1', 'CpicV2', 'cpic_v1', 'cpic_v2','cpicv1_dropout', 'CpicDropout','cpic_iberia','CpicIberia']
 
 model_urls = {
     'cpic_v1': 'http://geophysics.eas.gatech.edu/people/lijun/yews/models/cpic_v1_wenchuan-bdd92da2.pth',
@@ -347,7 +347,143 @@ class CpicDropout(nn.Module):
 
         return out
     
+class CpicIberia(nn.Module):
+    def __init__(self,dropout=0.08):
+        super().__init__()
+        if isinstance(dropout, float):
+            self.dropout = dropout
+        else:
+            raise ValueError("Dropout probability needs to be a float.")
+        # Add dropout at each layer
+        
+        # 2000 -> 1024
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(3, 16, kernel_size=5, stride=1, padding=26, bias=False),
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            # nn.Sigmoid(),
+            nn.MaxPool1d(2)
+        )
 
+        # 1024 -> 512
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(16, 32, kernel_size=5, stride=1, padding=2, bias=False),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            # nn.Sigmoid(),
+            nn.MaxPool1d(2)
+        )
+        
+        self.layer3 = nn.Sequential(
+            nn.Conv1d(32, 32, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            # nn.Sigmoid(),
+            #nn.MaxPool1d(2)
+        )
+
+        # 512 -> 256
+        self.layer4 = nn.Sequential(
+            nn.Conv1d(32, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 256 -> 128
+        self.layer5 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 128 -> 64
+        self.layer6 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 64 -> 32
+        self.layer7 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 32 -> 16
+        self.layer8 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 16 -> 8
+        self.layer9 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 8 -> 4
+        self.layer10 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 4 -> 2
+        self.layer11 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        # 2 -> 1
+        self.layer12 = nn.Sequential(
+            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=dropout),
+            nn.MaxPool1d(2)
+        )
+
+        self.fc = nn.Linear(64 * 1, 3)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.layer6(out)
+        out = self.layer7(out)
+        out = self.layer8(out)
+        out = self.layer9(out)
+        out = self.layer10(out)
+        out = self.layer11(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
     
 def cpic_v1(pretrained=False, progress=True, path=None, **kwargs):
     r"""Original CPIC model architecture from the
@@ -398,6 +534,26 @@ def cpicv1_dropout(pretrained=False, progress=True, dropout=0.08, path=None, **k
         progress (bool): If True, displays a progress bar of the download to stderr
     """
     model = CpicDropout(dropout=dropout, **kwargs)
+    if pretrained:
+        if path != None:
+            model.load_state_dict(load(path)['model'])
+        else:
+            state_dict = load_state_dict_from_url(model_urls['cpicv1_dropout'],
+                                                  progress=progress)
+            model.load_state_dict(state_dict)
+    return model
+
+def cpic_iberia(pretrained=False, progress=True, dropout=0.08, path=None, **kwargs):
+    r"""Original CPIC model architecture from the
+    `"Deep learning for ..." <https://arxiv.org/abs/1901.06396>`_ paper. The
+    pretrained model is trained on 60,000 Wenchuan aftershock dataset
+    demonstrated in the paper.
+
+    Args:
+        pretrained (bool): If True, returns a model pre-trained on Wenchuan)
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    model = CpicIberia(dropout=dropout, **kwargs)
     if pretrained:
         if path != None:
             model.load_state_dict(load(path)['model'])
