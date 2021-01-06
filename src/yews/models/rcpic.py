3363cfa7fd1567d15c9ae0e1aa113152e904e04e
Miguel Neves <36202483+miguelj-neves@users.noreply.github.com>
2021-01-06 00:29:28 +0000

    Update rcpic.py

--- src/yews/models/rcpic.py
+++ src/yews/models/rcpic.py
@@ -64,56 +64,60 @@ class RCpicV1(nn.Module):
             nn.ReLU(),
             nn.MaxPool1d(2),
         )
+        
+        self.fc = nn.Linear(64 * 64, 64)
+        
+        self.lstm = nn.LSTM(input_size=64,hidden_size=,num_layers=2,dropout=0,bidirectional=True)
 
         # 64 -> 32
-        self.layer7 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer7 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
         # 32 -> 16
-        self.layer8 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer8 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
         # 16 -> 8
-        self.layer9 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer9 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
         # 8 -> 4
-        self.layer10 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer10 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
         # 4 -> 2
-        self.layer11 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer11 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
         # 2 -> 1
-        self.layer12 = nn.Sequential(
-            nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.MaxPool1d(2),
-        )
+        #self.layer12 = nn.Sequential(
+        #    nn.Conv1d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+        #    nn.BatchNorm1d(64),
+        #    nn.ReLU(),
+        #    nn.MaxPool1d(2),
+        #)
 
-        self.fc = nn.Linear(64 * 1, 3)
+        #self.fc = nn.Linear(64 * 1, 3)
 
     def forward(self, x):
         out = self.layer1(x)
@@ -122,13 +126,14 @@ class RCpicV1(nn.Module):
         out = self.layer4(out)
         out = self.layer5(out)
         out = self.layer6(out)
-        out = self.layer7(out)
-        out = self.layer8(out)
-        out = self.layer9(out)
-        out = self.layer10(out)
-        out = self.layer11(out)
+        #out = self.layer7(out)
+        #out = self.layer8(out)
+        #out = self.layer9(out)
+        #out = self.layer10(out)
+        #out = self.layer11(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
+        out = self.lstm(out)
 
         return out
     
