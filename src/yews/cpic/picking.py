82fff8e5f4145751682e4d0ffba2dddeab0a2219
Miguel Neves <36202483+miguelj-neves@users.noreply.github.com>
2021-03-15 21:45:38 -0400

    Update picking.py

--- src/yews/cpic/picking.py
+++ src/yews/cpic/picking.py
@@ -7,9 +7,11 @@ from .utils import probs2cfs
 def pick_arrivals(cf):
     prom = cf.max()
     #mad = median_abs_deviation(cf)
+    median = np.median(cf)
+    mad = np.median(abs(cf-median))
     for i in range(2):
         #prom /= 2
-        peaks, properties = find_peaks(x=cf, height=0,
+        peaks, properties = find_peaks(x=cf, height=30*mad,
                                        distance=40, prominence=prom/2)
 
         if peaks.size > 0:
