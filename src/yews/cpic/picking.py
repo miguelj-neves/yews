44bd40c760428b225ee5283630bba21a41c13cc6
Miguel Neves <36202483+miguelj-neves@users.noreply.github.com>
2021-03-15 21:21:31 -0400

    Update picking.py

--- src/yews/cpic/picking.py
+++ src/yews/cpic/picking.py
@@ -10,8 +10,8 @@ def pick_arrivals(cf):
     mad = median_abs_deviation(cf)
     for i in range(10):
         #prom /= 2
-        peaks, properties = find_peaks(x=cf, height=20*mad,
-                                       distance=20, prominence=prom/4)
+        peaks, properties = find_peaks(x=cf, height=0.3,
+                                       distance=20, prominence=0.1)
 
         if peaks.size > 0:
             peak_prom = properties['prominences']
@@ -19,6 +19,30 @@ def pick_arrivals(cf):
             return peaks, confidences
 
     return (np.nan, np.nan)
+
+def pick_probs(waveform, fs, wl, model, transform, g=0.1, batch_size=None):
+    probs = compute_probs(model, transform, waveform,
+                          shape=[3, fs * wl],
+                          step=[1, int(g * fs)],
+                          batch_size=batch_size)
+
+    # compute cf
+    #cf_p, cf_s = probs2cfs(probs)
+
+    # find prominent local peaks
+    peaks_p, confidences_p = pick_arrivals(probs[1])
+    peaks_s, confidences_s = pick_arrivals(probs[2])
+
+    pick_results = {
+        'p': peaks_p * g + 5,
+        's': peaks_s * g + 5,
+        'p_conf': confidences_p,
+        's_conf': confidences_s,
+        'cf_p': cf_p,
+        'cf_s': cf_s
+    }
+
+    return pick_results
 
 
 def pick(waveform, fs, wl, model, transform, g=0.1, batch_size=None):
